d4ba6bb254f470dce24b0d50fb1105d9d8253f35
sennrdesign <sennadeturner@gmail.com>
2016-09-21 19:57:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var val = 1;
 function woordjes(){
   val++;
+  var para = document.getElementById("para");
   if(val < 11){
   var woordjes = document.getElementById("woordjes");
-  var para = document.getElementById("para");
   woordjes.innerHTML += '<input type="text" class="w-search" id="w'+val+'-nl" placeholder="franse woordjes..."/><input type="text" class="w-search" id="w'+val+'-fa" placeholder="nederlandse woordjes..."/><br/>';
+  }else{
+  para.style.color = "red";
+  para.innerHTML = "U heeft het maximale aantal woorden bereikt!";
   }
   }
