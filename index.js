71f4c0df4946a60c43436c34ed3a27f99da7954c
sennrdesign <sennadeturner@gmail.com>
2016-09-21 19:57:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var val = 1;
 function woordjes(){
   val++;
   var para = document.getElementById("para");
-  if(val < 11){
+  if(val < 12){
   var woordjes = document.getElementById("woordjes");
   woordjes.innerHTML += '<input type="text" class="w-search" id="w'+val+'-nl" placeholder="franse woordjes..."/><input type="text" class="w-search" id="w'+val+'-fa" placeholder="nederlandse woordjes..."/><br/>';
   }else{
