5c3a942c6fbd39cf59be0a4ab6cd75daf3e3b5a5
sennrdesign <sennadeturner@gmail.com>
2016-09-21 19:46:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+var val = 1;
 function woordjes(){
+  val++;
   var woordjes = document.getElementById("woordjes");
-  woordjes.innerHTML += '<input type="text" class="w-search" id="w2-nl" placeholder="franse woordjes..."/><input type="text" class="w-search" id="w2-nl" placeholder="nederlandse woordjes..."/><br/>';
+  woordjes.innerHTML += '<input type="text" class="w-search" id="w'+val+'-nl" placeholder="franse woordjes..."/><input type="text" class="w-search" id="w'+val+'-nl" placeholder="nederlandse woordjes..."/><br/>';
 }
