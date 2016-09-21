5722fdc547af441b138dea8326f33c65265eb020
sennrdesign <sennadeturner@gmail.com>
2016-09-21 19:51:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var val = 1;
 function woordjes(){
   val++;
+  if(val < 11){
   var woordjes = document.getElementById("woordjes");
+  var para = document.getElementById("para");
   woordjes.innerHTML += '<input type="text" class="w-search" id="w'+val+'-nl" placeholder="franse woordjes..."/><input type="text" class="w-search" id="w'+val+'-fa" placeholder="nederlandse woordjes..."/><br/>';
-}
+  }else{
+  para.innerHTML = "U heeft het maximale aantal woordjes bereikt";
+  }
+  }
